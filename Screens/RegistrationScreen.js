d76a3aa1d80d6cb06cba48cb6194db698bd73d95
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2023-04-01 14:17:59 +0300

    no work in phone and emulator ImagePicker

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -17,8 +17,8 @@ import * as ImagePicker from 'expo-image-picker';
 export default function RegistrationScreen() {
     const [image, setImage] = useState(null)
     
-    const pickImage = async () => {
-            console.log('fddd')
+    const pickImage = async (e) => {
+            console.log(e.target)
             // No permissions request is necessary for launching the image library
             let result = await ImagePicker.launchImageLibraryAsync({
                 mediaTypes: ImagePicker.MediaTypeOptions.All,
